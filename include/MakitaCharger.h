cf839df52de7f07090c9b4bff8dd43fd7c3a4fd1
hschack <hschack@gmail.com>
2023-06-20 21:26:44 +0200

    Virker_2

--- include/MakitaCharger.h
+++ include/MakitaCharger.h
@@ -3,6 +3,12 @@
 
 #include <Arduino.h>
 
+extern uint32_t currentMillis;
+extern float Filteredvoltage_A2;
+extern uint8_t ChargeBattery;
+extern uint8_t MAKITApin3status;
+extern uint32_t Printinterval;
+
 void MakitaChargerSetup();
 void MakitaChargerLoop();
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,24 @@
 #include <Arduino.h>
 #include "MakitaCharger.h"
 
+uint32_t currentMillis;
+float Filteredvoltage_A2 = 0.0f;
+uint8_t ChargeBattery = 0;
+uint8_t MAKITApin3status = 0;
+uint32_t Printinterval = millis();
+
 void setup() {
+  Serial.begin(115200);
+  while (!Serial)
+    ;
+  delay(100);
   MakitaChargerSetup();
 }
 
 void loop() {
   MakitaChargerLoop();
+    if (millis() - Printinterval >= 1000) {
+    Serial.println(Filteredvoltage_A2, 4);
+    Printinterval = millis();
+  }
 }
